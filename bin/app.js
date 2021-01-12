d68d5cdaa58dc6831d16c69b34718b19ede89a53
shrey079 <shreypatel079@gmail.com>
2021-01-11 23:23:39 -0500

    Changed functionality in the app.js file

--- bin/app.js
+++ bin/app.js
@@ -1,19 +1,23 @@
 const h1 = document.querySelector("h1")
+const h3 = document.querySelector('h3')
 const button = document.querySelector("button")
 
 function randInt() { 
-    return (Math.floor(Math.random() * 226)); 
+    return (Math.floor(Math.random() * 256)); 
 }
 
 function color() { 
+    h3.classList.add("hide")
     const r1 = randInt();
     const r2 = randInt();
     const r3 = randInt();
-    
+
     const str = `Rgb(${r1}, ${r2}, ${r3})`;
     const body = document.querySelector("body")
+    const button = document.querySelector("button")
     body.style.backgroundColor = str;
     h1.innerText = str;
+    button.style.backgroundColor = str; 
 }
 
 button.addEventListener('click', color);
